5aa3fd833ad00100e184483b94d96eca1fe20d42
David Brandt <chickensinspace@gmail.com>
2015-05-04 16:19:49 -0500

    sorted dropdowns

--- src/UpdateSelectMoonPage.java
+++ src/UpdateSelectMoonPage.java
@@ -48,7 +48,7 @@ public class UpdateSelectMoonPage {
 		//Just get all the names of the planets
 		try {
 			PreparedStatement getTableNames = conn
-					.prepareStatement("SELECT name FROM moon;");
+					.prepareStatement("SELECT name FROM moon ORDER BY name;");
 			getTableNames.execute();
 			ResultSet rs = getTableNames.getResultSet();
 			ResultSetMetaData rsmd = rs.getMetaData();
